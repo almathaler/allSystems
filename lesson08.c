fac8dacba0461ddbc9311c5dfbc2c15f3bd4b845
almathaler <athaler00@stuy.edu>
2019-09-19 11:01:01 -0400

    finally understand! little endian. also when u add 16 per bit, each individual bit increases by 16 but when those bits are grouped together to form the int, a 16 in the 2nd bit (for example, this is little endian remember) is actually +4026

--- lesson08.c
+++ lesson08.c
@@ -2,12 +2,58 @@
 #include <stdlib.h>
 
 int main(){
-  unsigned int i = 100; //100 in binary is: 01100100, in hex: 0x64
+  unsigned int i = 2200000000; //100 in binary is: 01100100, in hex: 0x64
   int *p = &i;
   char *c = &i;
   //s2
+
   printf("here is *p: %p, what it points to: %d \n", p, *p);
-  printf("here is *c: %p, what it points to: %c \n", c, *c);
+  printf("here is *c: %p, what it points to: %hhu \n", c, *c);
+  //s3
+  printf("\n");
+  printf("i in dec: %u (unsigned), signed: %d, i in hex: %x \n", i, i, i);
+  //s4
+  printf("\n");
+  int x;
+  for(x = 1; x<5; x++){
+      printf("printing out the %d byte of i: hex: %hhx, dec: %hhu\n", x, *c, *c);
+      c++;
+  }
+  c = &i;
+  //s5
+  printf("\n");
+  for (x = 1; x<5; x++){
+    *c = *c + 1;
+    printf("what c looks like: %p \n", c);
+    printf("incremented the %d byte of i. Now i looks like: hex: %x, dec: %u, signed: %d \n", x, i, i, i);
+    c++;
+  }
+  c = &i;
+  //s5.5
+  printf("\n");
+  for(x = 1; x<5; x++){
+      printf("printing out the %d byte of i after S5: hex: %hhx, dec: %hhu\n", x, *c, *c);
+      c++;
+  }
+  c = &i;
+  //s6
+  printf("\nADDING 16");
+  i = 2200000000;
+  printf("\n");
+  for (x = 1; x<5; x++){
+    *c = *c + 16;
+    printf("where c is: %p \n", c);
+    printf("incremented the %d byte of i. Now i looks like: hex: %x, dec: %u, signed: %d \n", x, i, i, i);
+    c++;
+  }
+  c = &i;
+  //s6.5
+  printf("\n");
+  for(x = 1; x<5; x++){
+      printf("printing out the %d byte of i after S5: hex: %hhx, dec: %hhu\n", x, *c, *c);
+      c++;
+  }
+  c = &i;
 
   return 0;
 }
