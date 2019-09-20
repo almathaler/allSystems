02bd5f6934391c4775d00555a0d254373f234797
almathaler <athaler00@stuy.edu>
2019-09-20 11:00:41 -0400

    have to do strchr

---
+++ lesson10.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  char *s = "hello";
+  char s1[10] = "hello";
+  printf("s points to: %p\n", s);
+  printf("s1 points to: %p\n", s1);
+  printf("address of \"hello\": %p\n", &"hello");
+
+  int x;
+  for (x=0; x<10; x++){
+    printf("This is what is in s1[%d]: %d \n", x, s1[x]);
+  }
+
+//
+
+    printf("Now onto the excersizes: \nStrcpy: \n");
+    char almaArray[10] = "alma";
+    char *alma = almaArray;
+    char copyAlmaArray[5];
+    char *copyAlma = copyAlmaArray;
+    strcpy(copyAlma, alma);
+    //howcome you don't need the dereference operator here?
+    printf("copyAlma: %s \n", copyAlma);
+    printf("almaArray: %s \n", alma);
+    //
+    printf("strcat: \n");
+    strcat(almaArray, copyAlma);
+    printf("almaArray: %s \n", alma);
+    //
+    printf("strcmp: \n");
+    printf("comparing almaArray (almaalma) and almaCopy (alma) : %d \n", strcmp(alma, copyAlma));
+    //
+    printf("strchr: \n");
+
+  return 0;
+}
